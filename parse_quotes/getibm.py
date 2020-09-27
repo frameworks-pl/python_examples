4a5736a5958ce910bb0907153a1c636a17759795
Janusz Grabis <janusz@frameworks.pl>
2020-09-27 21:11:51 +0200

    Work in progress

--- parse_quotes/getibm.py
+++ parse_quotes/getibm.py
@@ -1,7 +1,23 @@
 import json
-import urllib
+import urllib.request
+from datetime import datetime
+from pytz import timezone
 
-urllib.request.urlopen("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo")
+url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo"
+with urllib.request.urlopen(url) as response:
+    content = json.loads(response.read().decode())
+
+nowDateTime = datetime.now(timezone('US/Pacific'))
+print(nowDateTime)
+for k in content["Time Series (5min)"].keys():
+    #we need timezone for the math to work, but this will also move the date by seven hours
+    #to compensate for this, we will add those 7 hours back
+    currDateTime = datetime.strptime(k, '%Y-%m-%d %H:%M:%S').replace(tzinfo=timezone('US/Pacific')) + timedelta(hours=7)
+
+    print(currDateTime)
+    #print(nowDateTime + " " + currDateTime + " " + (nowDateTime - currDateTime).seconds/60)
+    #print(content["Time Series (5min)"][k]["4. close"])
+
 
 
 
